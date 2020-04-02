4a84d832078041650a24986ada838d27cf0dc115
Nick Smith <nick.smith@klaviyo.com>
2020-04-01 20:06:58 -0400

    download spark in sh 	modified:   Section_4/spark.sh

--- Section_4/spark.sh
+++ Section_4/spark.sh
@@ -4,7 +4,7 @@
 #https://spark.apache.org/docs/latest/submitting-applications.html
 
 
-#wget http://www.trieuvan.com/apache/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
+wget http://www.trieuvan.com/apache/spark/spark-2.4.5/spark-2.4.5-bin-hadoop2.7.tgz
 tar -xzf spark-2.4.5-bin-hadoop2.7.tgz
 
 MASTER=spark://10.5.0.2:7077 ./spark-2.4.5-bin-hadoop2.7/bin/spark-shell --master spark://10.5.0.2:7707
